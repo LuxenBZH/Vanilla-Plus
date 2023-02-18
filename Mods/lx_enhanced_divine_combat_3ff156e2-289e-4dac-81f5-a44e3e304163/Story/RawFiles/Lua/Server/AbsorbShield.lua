a5611504f00f8e60a98bc50bbbac053610e9346c
Luxen <luxen.zh@gmail.com>
2023-02-18 17:19:46 +0100

    New absorb shield system

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/AbsorbShield.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/AbsorbShield.lua
@@ -1,4 +1,4 @@
-Ext.Osiris.RegisterListener("NRD_OnStatusAttempt", 4, function(target, statusID, statusHandle, instigator)
+Ext.Osiris.RegisterListener("NRD_OnStatusAttempt", 4, "before", function(target, statusID, statusHandle, instigator)
     local entry = Ext.Stats.Get(statusID, nil, false, nil)
     if entry.StatsId and entry.StatsId ~= "" then
         local potion = Ext.Stats.Get(entry.StatsId)
