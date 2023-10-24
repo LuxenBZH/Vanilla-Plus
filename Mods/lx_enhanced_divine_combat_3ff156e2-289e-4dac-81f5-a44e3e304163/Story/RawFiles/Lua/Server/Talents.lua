cd726f835036e23b465b372716864b0d01a4ed0f
Luxen <luxen.zh@gmail.com>
2023-10-24 21:21:28 +0200

    Fixed Elemental Affinity on server side

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/Talents.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/Talents.lua
@@ -402,7 +402,6 @@ local ElementalAffinityAiFlags = {
 ---- Elemental Affinity rework
 --- @param e LuaGetSkillAPCostEvent
 Ext.Events.GetSkillAPCost:Subscribe(function(e)
-	if not e.Character.TALENT_ElementalAffinity then return end
 	local skill = e.Skill
 	local character = e.Character
 	local grid = e.AiGrid
@@ -412,29 +411,34 @@ Ext.Events.GetSkillAPCost:Subscribe(function(e)
     if character == nil or baseAP <= 0 then
         return baseAP, false
     end
-    
     local ability = skill.StatsObject.StatsEntry.Ability
     local elementalAffinity = false
-    if ability ~= "None" and baseAP > 1 and character.TALENT_ElementalAffinity and grid ~= nil and position ~= nil and radius ~= nil then
-        local aiFlags = ElementalAffinityAiFlags[ability]
-        if aiFlags ~= nil then
-            elementalAffinity = grid:SearchForCell(position[1], position[3], radius, aiFlags, -1.0)
-            if elementalAffinity then
-                baseAP = baseAP - 1
+	if character.TALENT_ElementalAffinity then
+        if ability ~= "None" and baseAP > 1 and  grid ~= nil and position ~= nil and radius ~= nil then
+            local aiFlags = ElementalAffinityAiFlags[ability]
+            if aiFlags ~= nil then
+                elementalAffinity = grid:SearchForCell(position[1], position[3], radius, aiFlags, -1.0)
+                if elementalAffinity then
+                    baseAP = baseAP - 1
+                end
             end
         end
-    end
 
-    local characterAP = 1
-    if skill.Requirement > 0 and skill.OverrideMinAP == "No" then
-        characterAP = Game.Math.GetCharacterWeaponAPCost(character)
-    end
+        local characterAP = 1
+        if skill.Requirement > 0 and skill.OverrideMinAP == "No" then
+            characterAP = Game.Math.GetCharacterWeaponAPCost(character)
+        end
 
-	if character.Character:GetStatus("COMBAT") and not character.Character:HasTag("VP_UsedElementalAffinity") then
-        e.ElementalAffinity = elementalAffinity
-	    e.AP = math.max(characterAP, baseAP)
-    else
-        e.ElementalAffinity = false
+        if not character.Character:HasTag("VP_UsedElementalAffinity") then
+            e.ElementalAffinity = elementalAffinity
+            e.AP = math.max(characterAP, baseAP)
+        elseif elementalAffinity then
+            e.AP = baseAP + 1
+            e.ElementalAffinity = true
+        else
+            e.AP = baseAP
+            e.ElementalAffinity = false
+        end
     end
 end)
 
