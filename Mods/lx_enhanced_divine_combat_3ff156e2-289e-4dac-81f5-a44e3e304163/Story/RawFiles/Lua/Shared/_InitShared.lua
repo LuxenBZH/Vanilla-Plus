50babe9368a724d8ea75e1bf384cdfff479b1f8d
Luxen <luxen.zh@gmail.com>
2024-01-28 14:37:29 +0100

    Removede custom attribute scaling formula from equipment

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Shared/_InitShared.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Shared/_InitShared.lua
@@ -62,27 +62,27 @@ VPlusId = "3ff156e2-289e-4dac-81f5-a44e3e304163"
 ---The new function to scale down damage attributes
 ---@param value integer PenaltyQualifier
 ---@param level integer
-local function NewMainAttributeScaling(value, level)
-    value = Data.PenaltyQualifier[value]
-    return (value >= 0 and 1 or -1) * math.ceil(value/10*level * Ext.ExtraData.AttributeBoostGrowth * 0.6)
-end
+-- local function NewMainAttributeScaling(value, level)
+--     value = Data.PenaltyQualifier[value]
+--     return (value >= 0 and 1 or -1) * math.ceil(value/10*level * Ext.ExtraData.AttributeBoostGrowth * 0.6)
+-- end
 
-local AttributesCorrection = {
-    "StrengthBoost",
-    "FinesseBoost",
-    "IntelligenceBoost"
-}
+-- local AttributesCorrection = {
+--     "StrengthBoost",
+--     "FinesseBoost",
+--     "IntelligenceBoost"
+-- }
 
-local StatTypeCorrection = {
-    "Armor",
-    "Weapon",
-    "Shield"
-}
+-- local StatTypeCorrection = {
+--     "Armor",
+--     "Weapon",
+--     "Shield"
+-- }
 
-Ext.Events.ModuleLoading:Subscribe(function(e)
-    for i,statType in pairs(StatTypeCorrection) do
-        for j,attribute in pairs(AttributesCorrection) do
-            Ext.Stats.SetLevelScaling(statType, attribute, NewMainAttributeScaling)
-        end
-    end
-end)
+-- Ext.Events.ModuleLoading:Subscribe(function(e)
+--     for i,statType in pairs(StatTypeCorrection) do
+--         for j,attribute in pairs(AttributesCorrection) do
+--             Ext.Stats.SetLevelScaling(statType, attribute, NewMainAttributeScaling)
+--         end
+--     end
+-- end)
