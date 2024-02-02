83196618309c7a1856a30a1e2df5c1a21609c1b8
Luxen <luxen.zh@gmail.com>
2024-02-02 20:56:53 +0100

    Fixed an oversight doing console error clutter related to custom CritMult handling

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Client/Sync/Statuses.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Client/Sync/Statuses.lua
@@ -1,6 +1,7 @@
 Ext.RegisterNetListener("VP_MultiplyStatus", function(channel, payload, user)
     local info = Ext.Json.Parse(payload)
-    local character = Ext.ClientEntity.GetCharacter(info.Character)
+    local character,err = xpcall(function() return Ext.ClientEntity.GetCharacter(info.Character) end, debug.traceback)
+    if not character then return end
     local status = Ext.ClientEntity.GetStatus(character, info.Status)
     status.StatsMultiplier = info.Multiplier
     --- Crit Multiplier particularity
@@ -15,7 +16,8 @@ Ext.RegisterNetListener("VP_MultiplyStatus", function(channel, payload, user)
 end)
 
 Ext.RegisterNetListener("VP_RecalculateStatusCritMultBonus", function(channel, payload, user)
-    local character = Ext.ClientEntity.GetCharacter(tonumber(payload))
+    local character,err = xpcall(function() Ext.ClientEntity.GetCharacter(info.Character) end, debug.traceback)
+    if not character then return end
     local _,critMult = Data.Math.ComputeStatIntegerFromStatus(character, "VP_CriticalMultiplier")
     character.Stats.MainWeapon.DynamicStats[1].CriticalDamage = character.Stats.MainWeapon.StatsEntry.CriticalDamage + critMult
 end)
