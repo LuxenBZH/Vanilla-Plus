7e839760dff9db61955f6f3ae8984e4005744db7
Luxen <luxen.zh@gmail.com>
2024-06-30 03:02:25 +0200

    Vertcasting and laddercasting better fixes at cursor level

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Client/Fixes/ClientCasting.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Client/Fixes/ClientCasting.lua
@@ -1,63 +1,26 @@
--- Ext.Events.SessionLoaded:Subscribe(function(e)
---   Ext.Events.UIInvoke:Subscribe(function(e)
---     if e.UI.Type == Ext.UI.TypeID.textDisplay and e.Function == "addText" and e.When == "Before" then
---       local character = Helpers.GetPlayerManagerCharacter()
---       if character and character.SkillManager.CurrentSkill ~= null then
---         --- Vertcasting check
---         local position = Ext.ClientUI.GetPickingState().WalkablePosition
---         if math.abs(Ext.GetAiGrid():GetCellInfo(position[1], position[3]).Height - position[2]) > 2 then
---           local cc = Ext.UI.GetCursorControl()
---           local td = Ext.UI.GetByHandle(cc.TextDisplayUIHandle)
---           local ui = e.UI --[[@as EclUICursorInfo]]
---           local distance = string.match(td.Text, "[0-9]+.[0-9]m")
---           e:PreventAction()
---           if distance then
---             ui.Text = '<font color=\"#C80030\">'..Ext.L10N.GetTranslatedString("hdfbc0f44g7f3dg4985g9621g25cba41922e0")..'!</font><br>'..distance
---             ui:GetRoot().addText('<font color=\"#C80030\">'..Ext.L10N.GetTranslatedString("hdfbc0f44g7f3dg4985g9621g25cba41922e0")..'!</font><br>'..distance, e.Args[2], e.Args[3])
---           end 
---         end
---       end
---     end
---   end)
-
-  ---@param e EclLuaInputEvent
-  -- Ext.Events.InputEvent:Subscribe(function(e)
-  --   local character = Helpers.GetPlayerManagerCharacter()
-  --   local state = Ext.ClientUI.GetPickingState()
-  --   if not state then return end
-  --   local position = state.WalkablePosition
-  --   if e.Event.Release and e.Event.EventId == 4 and character and character.SkillManager.CurrentSkill ~= null and math.abs(Ext.GetAiGrid():GetCellInfo(position[1], position[3]).Height - position[2]) > 2 then
-  --     Ext.Net.PostMessageToServer("LX_VertcastingDecast", tostring(character.NetID))
-  --     if PreviousLadder then
-  --       local ladder = Ext.Entity.GetItem(PreviousLadder)
-  --       ladder.CurrentTemplate.CanClickThrough = false
-  --       ladder.CanUse = true
-  --     end
-  --   end
-  -- end)
-
-  Ladders = {}
-  CastWatcher = 0
-
-  ---- Ladder casting fixes
---   Ext.RegisterNetListener("LX_LaddercastFixEnter", function(channel, payload)
---     local ladders = Ext.Json.Parse(payload)
---     for i, netID in pairs(ladders) do
---       local ladder = Ext.Entity.GetItem(netID)
---       ladder.CurrentTemplate.CanClickThrough = true
---       ladder.CanUse = false
---       table.insert(Ladders, netID)
---     end
---     CastWatcher = Ext.Events.Tick:Subscribe(function(e)
---       local character = Helpers.GetPlayerManagerCharacter()
---       if character and character.SkillManager and character.SkillManager.CurrentSkill == null then
---         for i, netID in pairs(Ladders) do
---           local ladder = Ext.Entity.GetItem(netID)
---           ladder.CurrentTemplate.CanClickThrough = false
---           ladder.CanUse = true
---         end
---         Ext.Events.Tick:Unsubscribe(CastWatcher)
---       end
---     end)
---   end)
--- end)
+Ext.ClientBehavior.Skill.AddGlobal(function()
+  local EclCustomSkillState = {}
+  ---@param ev CustomSkillEventParams
+  ---@param skillState EclSkillState
+  ---@param targetHandle ComponentHandle
+  ---@param targetPos vec3
+  ---@param snapToGrid boolean
+  ---@param fillInHeight boolean
+  ---@return number
+  function EclCustomSkillState:ValidateTarget(ev, skillState, targetHandle, targetPos, snapToGrid, fillInHeight)
+    if math.abs(Ext.GetAiGrid():GetCellInfo(targetPos[1], targetPos[3]).Height - targetPos[2]) > 2 then
+      ev.PreventDefault = true
+			ev.StopEvent = true
+      return 8
+    elseif Ext.Utils.IsValidHandle(targetHandle) then
+      local target = Ext.ClientEntity.GetGameObject(targetHandle)
+      if Helpers.IsItem(target) and target.IsLadder then
+        ev.PreventDefault = true
+			  ev.StopEvent = true
+        return 4
+      end
+    end
+    return 0
+  end
+  return EclCustomSkillState
+end)
