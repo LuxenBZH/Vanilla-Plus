7ed71985d030b5424f9710bf4084dcd2f893bbb0
Luxen <luxen.zh@gmail.com>
2024-09-09 20:54:17 +0200

    Fixed an issue related to timers deletion and added named timers

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Shared/Helpers/Timers.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Shared/Helpers/Timers.lua
@@ -17,11 +17,29 @@ function Helpers.Timer.Start(time, callback, repeatCount, ...)
     })
 end
 
----@param timer table
+---@param name string
+---@param time number Time in milliseconds
+---@param callback function
+---@param repeatCount number|nil
+function Helpers.Timer.StartNamed(name, time, callback, repeatCount, ...)
+    table.insert(Helpers.Timer.ActiveTimers, {
+        Name = name,
+        Time = time,
+        Remaining = time,
+        Callback = callback,
+        RepeatCount = repeatCount or 0,
+        Params = {...}
+    })
+end
+
+---@param timer table|string
 function Helpers.Timer.Delete(timer)
     local success = false
     for i, activeTimer in pairs(Helpers.Timer.ActiveTimers) do
-        if timer == activeTimer then
+        if type(timer) == "table" and timer == activeTimer then
+            table.remove(Helpers.Timer.ActiveTimers, i)
+            success = true
+        elseif type(timer) == "string" and activeTimer.Name == timer then
             table.remove(Helpers.Timer.ActiveTimers, i)
             success = true
         end
@@ -34,15 +52,16 @@ end
 Ext.Events.Tick:Subscribe(function()
     local time = Ext.Utils.MonotonicTime()
     local timeDelta = time - Helpers.Timer.CurrentTime
-
     for i, timer in pairs(Helpers.Timer.ActiveTimers) do
         if timeDelta >= timer.Remaining then
             timer.Callback(table.unpack(timer.Params))
-            if timer.RepeatCount > 0 then
+            if Helpers.Timer.ActiveTimers[i] and timer.RepeatCount > 0 then
                 Helpers.Timer.ActiveTimers[i].RepeatCount = timer.RepeatCount - 1
                 Helpers.Timer.ActiveTimers[i].Remaining = timer.Time
             else
-                Helpers.Timer.Delete(timer)
+                if Helpers.Timer.ActiveTimers[i] then
+                    Helpers.Timer.Delete(timer)
+                end
             end
         else
             Helpers.Timer.ActiveTimers[i].Remaining = Helpers.Timer.ActiveTimers[i].Remaining - timeDelta
