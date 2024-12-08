a7b3d3c0c21b078d1455c335562b339b49aa0a10
Luxen <luxen.zh@gmail.com>
2024-12-08 15:34:20 +0100

    VFX for Mace weapon art

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Shared/Systems/Weapons/WeaponArts.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Shared/Systems/Weapons/WeaponArts.lua
@@ -1,23 +1,106 @@
+--------------
+--- CLIENT ---
+--------------
 if Ext.IsClient() then
+    local impactEffect
+    local visual
+    local targeted = {}
+
+    Ext.RegisterNetListener("LX_WA_MaceSendEffect", function(channel, payload)
+        impactEffect = tonumber(payload)
+        local item = Ext.ClientEntity.GetItem(tonumber(payload))
+        visual = Ext.ClientVisual.CreateOnItem(item.WorldPos, item) ---@type EclLuaVisualClientMultiVisual
+        visual:ParseFromStats("VP_FX_Target_Circle_3x", "")
+        visual = visual.Handle
+    end)
 	Ext.ClientBehavior.Skill.AddGlobal(function()
 		local EclCustomSkillState = {}
+
 		---@class ev CustomSkillEventParams
 		---@param skillState EclSkillState
 		---@param inputEvent InputEvent
 		---@return boolean
 		function EclCustomSkillState:EnterBehaviour(ev,skillState)
             local character = Ext.ClientEntity.GetCharacter(skillState.CharacterHandle)
-            if character:GetStatus("LX_WA_RANGEPLUS") then
-                local statEntry = Ext.Stats.Get(string.gsub(skillState.SkillId, "%_%-1", ""))
-                if statEntry.UseWeaponDamage == "No" then return false end
+            local statEntry = Ext.Stats.Get(string.gsub(skillState.SkillId, "%_%-1", ""))
+            if character:GetStatus("LX_WA_RANGEPLUS") and statEntry.UseWeaponDamage == "Yes" then
                 if skillState.Type == "Target" or skillState.Type == "MultiStrike" then
                     skillState.TargetRadius = skillState.TargetRadius *1.35
                 elseif skillState.Type == "Shout" then
                     skillState.AreaRadius = skillState.AreaRadius * 1.35
                 end
+            elseif character:GetStatus("LX_WA_MACE") and statEntry.UseWeaponDamage == "Yes" and (skillState.Type == "Zone" or skillState.Type == "Rush") then
+                Ext.Net.PostMessageToServer("LX_WA_MaceCreateEffect", tostring(character.NetID))
+                Ext.Net.PostMessageToServer("LX_CreateCustomTargetEffect", Ext.Json.Stringify({
+                    UserID = character.UserID,
+                    Radius = 3,
+
+                }))
+                Helpers.AuraTargeting.Client.ApplyTargeting(skillState.Type == "Rush" and Ext.ClientUI.GetPickingState().WalkablePosition or character.WorldPos, 3, false, false, true, true, character)
+                -- visual:AddVisual("")
+                --- note: create visual by creating an invisible item with the template GUID "9a0c0892-64ff-4e2c-9137-322efe4946c2"
+                --- Once created on the server, create a visual with Ext.ClientVisual.Create
+                --- Then apply an FX by using visual:ParseFromStats("RS3_FX_UI_Target_Circle_01", "") (it says FromStats but it takes the FX name directly)
+                --- Then move the item by change the EclItem.Translate coordinate on tick
+                --- Don't forget to delete the item once the skill is decast or casted.
             end
 			return false
 		end
+
+        local once = false
+        
+        ---@param ev CustomSkillEventParams
+        ---@param skillState EclSkillState
+        ---@param targetHandle ComponentHandle
+        ---@param targetPos vec3
+        ---@param snapToGrid boolean
+        ---@param fillInHeight boolean
+        ---@return CustomSkillState.ValidateTargetResult
+        function EclCustomSkillState:ValidateTargetSight(ev, skillState, targetPos)
+            local character = Ext.ClientEntity.GetCharacter(skillState.CharacterHandle)
+            local statEntry = Ext.Stats.Get(string.gsub(skillState.SkillId, "%_%-1", "")) 
+            -- if not once and skillState.State ~= "Init" then
+            --     _DS(skillState)
+            --     _DS(targetPos)
+            --     once = true
+            -- end
+            if character:GetStatus("LX_WA_MACE") and statEntry.UseWeaponDamage == "Yes" and (skillState.Type == "Zone" or skillState.Type == "Rush") then
+                if impactEffect and visual then
+                    Ext.ClientEntity.GetItem(impactEffect).Translate = Ext.ClientUI.GetPickingState().WalkablePosition
+                end
+                
+                -- local pos = skillState.Type == "Rush" and Ext.ClientUI.GetPickingState().WalkablePosition or character.WorldPos
+                -- local inRange = Helpers.GetCharactersAroundPosition(pos[1], pos[2], pos[3], 3)
+                -- for netID,effect in pairs(targeted) do
+
+                --     if Ext.Math.Distance(pos, Ext.ClientEntity.GetCharacter(netID).WorldPos) > 3 then
+                --         Ext.ClientVisual.Get(effect):Delete()
+                --         targeted[netID] = nil
+                --     end
+                -- end
+                -- for i,char in pairs(inRange) do
+                --     if not targeted[char.NetID] then
+                --         local arrowBillboard = Ext.ClientVisual.CreateOnCharacter({char.WorldPos[1], char.WorldPos[2], char.WorldPos[3]}, char) ---@type EclLuaVisualClientMultiVisual
+                --         -- local arrowBillboard = Ext.ClientVisual.Create({char.WorldPos[1], char.WorldPos[2]+char.AI.AIBoundsHeight*1.15, char.WorldPos[3]}) ---@type EclLuaVisualClientMultiVisual
+                --         arrowBillboard:ParseFromStats("RS3_FX_UI_Icon_TriangleDown_01_Yellow_Active", "")
+                --         targeted[char.NetID] = arrowBillboard.Handle
+                --     end
+                -- end
+            end
+            return 1
+        end
+
+        -- ---@param ev CustomSkillEventParams
+        -- ---@param skillState EclSkillState
+        -- function EclCustomSkillState:TickAction(ev, skillState)
+        --     local character = Ext.ClientEntity.GetCharacter(skillState.CharacterHandle)
+        --     local statEntry = Ext.Stats.Get(string.gsub(skillState.SkillId, "%_%-1", "")) ---@type StatEntrySkillData
+        --     if character:GetStatus("LX_WA_MACE") and statEntry.SkillType == "Rush" and statEntry.UseWeaponDamage == "Yes" then
+        --         if impactEffect and visual then
+        --             impactEffect.Translate = skillState.TargetPosition
+        --         end
+        --     end
+        -- end
 
         ---@param ev CustomSkillEventParams
         ---@param skillState EclSkillState
@@ -33,8 +116,33 @@ if Ext.IsClient() then
                     }))
                 end
             end
+            Helpers.AuraTargeting.Client.Stop()
+            if impactEffect then
+                if visual then
+                    Ext.ClientVisual.Get(visual):Delete()
+                    visual = nil
+                end
+                Ext.Net.PostMessageToServer("LX_DeleteEffectItem", tostring(impactEffect))
+                impactEffect = nil
+            end
             return false
         end
+
+        ---@param ev CustomSkillEventParams
+        ---@param skillState EclSkillState
+        function EclCustomSkillState:ExitBehaviour(ev, skillState)
+            Helpers.AuraTargeting.Client.Stop()
+            if impactEffect then
+                if visual then
+                    Ext.ClientVisual.Get(visual):Delete()
+                    visual = nil
+                end
+                Ext.Net.PostMessageToServer("LX_DeleteEffectItem", tostring(impactEffect))
+                impactEffect = nil
+            end
+            return false
+        end
+
 		return EclCustomSkillState
 	end)
 
@@ -79,6 +187,9 @@ if Ext.IsClient() then
     end)
 end
 
+--------------
+--- SERVER ---
+--------------
 if Ext.IsServer() then
     Ext.RegisterNetListener("LX_WA_RangePlusShout", function(channel, payload, ...)
         local info = Ext.Json.Parse(payload)
@@ -117,6 +228,17 @@ if Ext.IsServer() then
             end, 210, character.MyGuid)
             Helpers.Character.AddSkillCooldown(character, skill, math.ceil(statEntry.Cooldown*0.30)*6)
         end
+    end)
+
+    Ext.RegisterNetListener("LX_WA_MaceCreateEffect", function(channel, payload)
+        local character = Ext.ServerEntity.GetCharacter(tonumber(payload))
+        local item = Ext.ServerEntity.GetItem(CreateItemTemplateAtPosition("9a0c0892-64ff-4e2c-9137-322efe4946c2", character.WorldPos[1], character.WorldPos[2], character.WorldPos[3]))
+        Helpers.Timer.Start(99, function(characterGUID, itemNetID) Ext.Net.PostMessageToClient(characterGUID, "LX_WA_MaceSendEffect", tostring(itemNetID)) end, nil, character.MyGuid, item.NetID)
+    end)
+
+    Ext.RegisterNetListener("LX_DeleteEffectItem", function(channel, payload)
+        local item = Ext.ServerEntity.GetItem(tonumber(payload))
+        ItemRemove(item.MyGuid)
     end)
 end
 
