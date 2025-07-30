ec87d706e781ffb0ee0aca3dacb9b9b1ee1d68cc
Luxen <luxen.zh@gmail.com>
2025-07-30 02:52:44 +0200

    Fixed an issue where the client vertcasting fix would drastically reduce client FPS while casting a skill with a SurfaceChange property

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Client/Fixes/ClientCasting.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Client/Fixes/ClientCasting.lua
@@ -1,5 +1,24 @@
-Ext.ClientBehavior.Skill.AddGlobal(function()
+local function PreventClientVertcasting()
   local EclCustomSkillState = {}
+  local skillProperties
+  ---There is a bug where skills with a SurfaceChange property will trigger ValidateTarget way more and kill client FPS
+  ---A workaround is to remove the SurfaceChange property temporarily from client side while casting the skill
+  ---@param ev CustomSkillEventParams
+  ---@param skillState EclSkillState
+  function EclCustomSkillState:Enter(ev, skillState)
+    local skill = Helpers.GetFormattedSkillID(skillState.SkillId)
+    local statEntry = Ext.Stats.Get(skill)
+    skillProperties = statEntry.SkillProperties
+    local cleanProperties = {}
+    for i, property in pairs(statEntry.SkillProperties) do
+      if property.Type ~= "SurfaceChange" then
+        table.insert(cleanProperties, property)
+      end
+    end
+    statEntry.SkillProperties = cleanProperties
+    return false
+  end
+
   ---@param ev CustomSkillEventParams
   ---@param skillState EclSkillState
   ---@param targetHandle ComponentHandle
@@ -8,20 +27,65 @@ Ext.ClientBehavior.Skill.AddGlobal(function()
   ---@param fillInHeight boolean
   ---@return number
   function EclCustomSkillState:ValidateTarget(ev, skillState, targetHandle, targetPos, snapToGrid, fillInHeight)
-    if math.abs(Ext.GetAiGrid():GetCellInfo(targetPos[1], targetPos[3]).Height - targetPos[2]) > 2 then
+    if math.abs(Ext.ClientEntity.GetAiGrid():GetHeight(targetPos[1], targetPos[3]) - targetPos[2]) > 2 then
       ev.PreventDefault = true
-			ev.StopEvent = true
+      ev.StopEvent = true
       return 8
-    elseif Ext.Utils.IsValidHandle(targetHandle) then
+    end
+    return 0
+  end
+
+  ---Make sure to restore the original skill properties before hit since it's client-driven
+  ---@param ev CustomSkillEventParams
+  ---@param skillState EclSkillState
+  function EclCustomSkillState:EnterAction(ev, skillState)
+    if #skillProperties > 0 then
+      local skill = Helpers.GetFormattedSkillID(skillState.SkillId)
+      local statEntry = Ext.Stats.Get(skill)
+      statEntry.SkillProperties = skillProperties
+    end
+    return false
+  end
+
+  ---@param ev CustomSkillEventParams
+  ---@param skillState EclSkillState
+  function EclCustomSkillState:Finish(ev, skillState)
+    if #skillProperties > 0 then
+      local skill = Helpers.GetFormattedSkillID(skillState.SkillId)
+      local statEntry = Ext.Stats.Get(skill)
+      statEntry.SkillProperties = skillProperties
+    end
+    return false
+  end
+  return EclCustomSkillState
+end
+
+local function PreventLadderCasting()
+  local EclCustomSkillState = PreventClientVertcasting()
+  
+  ---@param ev CustomSkillEventParams
+  ---@param skillState EclSkillState
+  ---@param targetHandle ComponentHandle
+  ---@param targetPos vec3
+  ---@param snapToGrid boolean
+  ---@param fillInHeight boolean
+  ---@return number
+  function EclCustomSkillState:ValidateTarget(ev, skillState, targetHandle, targetPos, snapToGrid, fillInHeight)
+    if Ext.Utils.IsValidHandle(targetHandle) then
       local target = Ext.ClientEntity.GetGameObject(targetHandle)
       -- Ladder should still be targetable if they are meant to be destroyed (if they have an HP bar)
       if Helpers.IsItem(target) and target.IsLadder and target.StatsFromName.StatsEntry.Vitality == -1 then
         ev.PreventDefault = true
-			  ev.StopEvent = true
+        ev.StopEvent = true
         return 4
       end
     end
     return 0
   end
+
   return EclCustomSkillState
-end)
+end
+
+Ext.ClientBehavior.Skill.AddByType("Target", PreventClientVertcasting)
+Ext.ClientBehavior.Skill.AddByType("ProjectileStrike", PreventClientVertcasting)
+Ext.ClientBehavior.Skill.AddByType("Projectile", PreventLadderCasting)
