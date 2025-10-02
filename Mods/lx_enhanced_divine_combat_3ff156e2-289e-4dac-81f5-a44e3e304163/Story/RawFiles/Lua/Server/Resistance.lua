d63463ea295651eb294ac8b2ff583244da3519c4
Luxen <luxen.zh@gmail.com>
2025-10-02 02:54:38 +0200

    Fixes and updated resistance calculation for the new Ingress behavior

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/Resistance.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/Resistance.lua
@@ -2,27 +2,22 @@
 --- @param type string DamageType enumeration
 local function GetResistance(character, damageType)
 	local cap = character.MaxResistance
-	local pen = Data.Math.ComputeCharacterIngress(character.Character) -- Resistance - penetration value
+	local resistance = character[tostring(damageType).."Resistance"]
 	if damageType == "None" or damageType == "Chaos" then
 		damageType = "Custom" 
 	end
-	-- New V+ resistance cap behavior
-	if pen then
-		local resCap = FindTag(Ext.GetCharacter(character.MyGuid), "DGM_ResCap"..tostring(damageType)) or "100"
-		local typeCap = tonumber(string.gsub(resCap, ".*_", "")[1])
-		if typeCap then
-			cap = cap + typeCap
-		end
-		if character["Base"..tostring(damageType).."Resistance"] > cap then
-			return character["Base"..tostring(damageType).."Resistance"]
-		elseif pen > cap then
-			return cap
-		else
-			return pen
-		end
-	-- Old behavior, still available
+	local resCap = FindTag(Ext.GetCharacter(character.MyGuid), "DGM_ResCap"..tostring(damageType)) or character.MaxResistance
+	local typeCap = tonumber(string.gsub(resCap, ".*_", "")[1])
+	if typeCap then
+		cap = cap + typeCap
+	end
+	-- If the base resistance is higher than the cap, let it be
+	if character["Base"..tostring(damageType).."Resistance"] > cap then
+		return character["Base"..tostring(damageType).."Resistance"]
+	elseif resistance > cap then
+		return cap
 	else
-		return character[tostring(damageType).."Resistance"]
+		return resistance
 	end
 end
 
@@ -33,19 +28,7 @@ local function GetResistanceBypassValue(character)
 	if not character then
 		return 0
 	end
-	local strength = character.Strength
-	local intelligence = character.Intelligence
-	local bypassValue = math.min(intelligence - Ext.ExtraData.AttributeBaseValue, (strength - Ext.ExtraData.AttributeBaseValue) * Ext.ExtraData.DGM_StrengthResistanceIgnore)
-	if character.MainWeapon then
-		if character.MainWeapon.WeaponType == "Staff" then
-			bypassValue = bypassValue + 10
-		elseif character.MainWeapon.WeaponType == "Wand" then
-			bypassValue = bypassValue + 5
-		end
-	end
-	if character.OffHandWeapon and character.OffHandWeapon.WeaponType == "Wand" then
-		bypassValue = bypassValue + 5
-	end
+	local bypassValue = Data.Math.ComputeCharacterIngress(character.Character)
 	if character.TALENT_Haymaker then
 		bypassValue = bypassValue + (character.Wits-Ext.ExtraData.AttributeBaseValue)*Ext.ExtraData.CriticalBonusFromWits
 	end
@@ -62,17 +45,16 @@ local function CustomApplyHitResistances(character, damageList, attacker)
 		local bypassValue = GetResistanceBypassValue(attacker)
 		-- Ext.Print("Resistance bypass value:",bypassValue)
 		-- Ext.Print(resistance)
-		if originalResistance ~= nil and originalResistance > 0 and originalResistance < 100 and bypassValue > 0 then
+		if originalResistance ~= nil and originalResistance > 50 and originalResistance < 100 and bypassValue > 0 then
             -- resistance = GetResistance(character, damage, originalResistance - bypassValue)
 			resistance = originalResistance - bypassValue
-			if resistance < 0 then
-				resistance = 0
+			if resistance < 50  then
+				resistance = 50
 			elseif resistance > originalResistance then
 				resistance = originalResistance
 			end
-		-- else
-			-- resistance = 1
 		end
+		_P(tostring(damage.DamageType).." Resistance: "..tostring(resistance))
         damageList:Add(damage.DamageType, math.floor(damage.Amount * -resistance / 100.0))
     end
 end
