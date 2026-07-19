86acbc4f8f7123e78818481b89bbf2fb29ef430d
Luxen <luxen.zh@gmail.com>
2026-07-19 04:10:13 +0200

    Added status effectiveness handling logic on status application

--- Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/Systems/Mechanics/StatusApplicationLogic.lua
+++ Mods/lx_enhanced_divine_combat_3ff156e2-289e-4dac-81f5-a44e3e304163/Story/RawFiles/Lua/Server/Systems/Mechanics/StatusApplicationLogic.lua
@@ -1,12 +1,21 @@
 ---@param e EsvLuaBeforeStatusApplyEvent
 Ext.Events.BeforeStatusApply:Subscribe(function(e)
-	if e.Status.StatusType == "DAMAGE" then
-		local instigator = Ext.Utils.IsValidHandle(e.Status.StatusSourceHandle) and Ext.ServerEntity.GetGameObject(e.Status.StatusSourceHandle) or nil
-		local target = Ext.ServerEntity.GetGameObject(e.Status.TargetHandle)
-		local applied
+	local instigator = Ext.Utils.IsValidHandle(e.Status.StatusSourceHandle) and Ext.ServerEntity.GetGameObject(e.Status.StatusSourceHandle) or nil
+	local target = Ext.ServerEntity.GetGameObject(e.Status.TargetHandle)
+	local applied = target:GetStatus(e.Status.StatusId)
+	--- Status effectiveness application logic
+	--- WARNING: this only work if the StatsMultiplier logic is done on BeforeStatusApply. Any change done once the status is applied will ignore this logic!
+	if applied then
+		if applied.StatsMultiplier > e.Status.StatsMultiplier then
+			e.PreventStatusApply = true
+			applied.CurrentLifeTime = math.max(applied.CurrentLifeTime, e.Status.CurrentLifeTime)
+			applied.RequestClientSync = true
+		end
+	end
+	--- Wits damage scaling + snapshotting
+	if e.Status.StatusType == "DAMAGE" and not e.PreventStatusApply then
 		local witsMultiplier = 1.0
 		if Helpers.IsCharacter(instigator) then
-			applied = target:GetStatus(e.Status.StatusId)
 			witsMultiplier = 1.0 + (instigator.Stats.Wits - Ext.ExtraData.AttributeBaseValue) * Ext.ExtraData.DGM_WitsDotBonus / 100
 		end
 		if applied then
